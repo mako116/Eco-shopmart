572411f53a7fbbcccffd3277a953b350b219a47f
emmanuel <nwakwueemmanuel1@gmail.com>
2024-04-10 14:40:43 +0100

    commited

---
+++ lib/Widgets/CartBottomNavbarbar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CartBottomNavbarbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/CartPage.dart
+++ lib/pages/CartPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shoppingmart/Widgets/CartAppBar.dart';
+import 'package:shoppingmart/Widgets/CartBottomNavbarbar.dart';
 import 'package:shoppingmart/Widgets/CartItemsSamples.dart';
 
 class CartPage extends StatelessWidget {
@@ -18,11 +19,45 @@ class CartPage extends StatelessWidget {
                     topLeft: Radius.circular(35),
                     topRight: Radius.circular(35))),
             child: Column(
-              children: [CartItemsSamples()],
+              children: [
+                CartItemsSamples(),
+                Container(
+                  // decoration:
+                  //     BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  // ),
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xff4c53a5),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          "add Coupon Code",
+                          style: TextStyle(
+                              color: Color(0xff4c53a5),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          )
+          ),
         ],
       ),
+      bottomNavigationBar: CartBottomNavbarbar(),
     );
   }
 }
